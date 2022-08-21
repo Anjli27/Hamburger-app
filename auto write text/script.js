448acd09b70e1a3734529b31d3549df22b08bc4c
Anjli <singlaanjali027@gmail.com>
2022-08-21 10:32:08 +0530

    initial

---
+++ auto write text/script.js
@@ -0,0 +1,15 @@
+const text = "Happy to be here keep going!! <3"
+
+let index = 0;
+
+function writeText() {
+    document.body.innnerText = text.slice(0, index);
+
+    index++;
+
+    if (index > text.length - 1) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
