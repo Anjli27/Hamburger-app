c3c01a5a6587c5872adba403b2e82c01eac53147
Anjli <singlaanjali027@gmail.com>
2022-08-21 11:13:41 +0530

    commit

--- auto write text/script.js
+++ auto write text/script.js
@@ -1,4 +1,4 @@
-const text = "Happy to be here keep going!! <3"
+const text = "Happy to be here keep going!! <3";
 
 let index = 0;
 
@@ -7,7 +7,7 @@ function writeText() {
 
     index++;
 
-    if (index > text.length - 1) {
+    if (index > text.length) {
         index = 0;
     }
 }
