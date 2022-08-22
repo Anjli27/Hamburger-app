6dc8f68aedd328b92655a26c4be3d17eb068ab98
Anjli <singlaanjali027@gmail.com>
2022-08-22 16:56:04 +0530

    commit
    zoom effect

--- auto write text/script.js
+++ auto write text/script.js
@@ -3,7 +3,7 @@ const text = "Happy to be here keep going!! <3";
 let index = 0;
 
 function writeText() {
-    document.body.innnerText = text.slice(0, index);
+    document.body.innerText = text.slice(0, index);
 
     index++;
 
